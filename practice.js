10bf5d2cd88551b45fa59ab555b068c74c27804e
Matthew Kastner <matthewkastner@gmail.com>
2019-07-24 14:24:20 -0600

    finished assignment 3

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(arr, cb){
+  cb(arr[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr, cb){
+  cb(arr[arr.length - 1])
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1, num2, cb){
+  cb(num1 * num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +94,15 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr, name, cb){
+  let cond = false
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === name){
+      cond = true
+    }
+  }
+  cb(cond)
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +124,16 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(arr,cb){
+  for(let i = 0; i < arr.length; i ++){
+    for(let j = i+1; j < arr.length; j ++){
+      if (arr[i] === arr[j]){
+        arr.splice(j,1)
+      }
+    }
+  }
+  return cb(arr)
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +151,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(arr, cb){
+  for(let i = 0; i < arr.length; i++){
+    cb(arr[i], i)
+  }
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,6 +173,14 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById(arr, id, cb){
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].id === id){
+      return cb(arr[i])
+    }
+  }
+  
+}
 
 // Do not edit the code below.
 var users = [
